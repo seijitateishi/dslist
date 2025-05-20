e74f6bc9997115f9d66a74f5e053b0471ac2bca4
seiji <seijitateishi@hotmail.com>
2025-05-20 16:13:02 -0300

    game seed

--- src/main/java/com/seiji/dslist/entities/Game.java
+++ src/main/java/com/seiji/dslist/entities/Game.java
@@ -15,20 +15,24 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -36,6 +40,14 @@ public class Game {
 
     public Long getId() {
         return id;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public void setId(Long id) {
